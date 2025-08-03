209334eee4643e6e607e76f3ab0bd2fde314f390
Kingsword <kingsword09@gmail.com>
2025-08-03 08:42:26 +0800

    feat: add postgres tools pg_basebackup

--- binding.cjs
+++ binding.cjs
@@ -394,6 +394,7 @@ if (!nativeBinding) {
 
 module.exports = nativeBinding
 module.exports.ConnectionInfo = nativeBinding.ConnectionInfo
+module.exports.PgBasebackupTool = nativeBinding.PgBasebackupTool
 module.exports.PgDumpallTool = nativeBinding.PgDumpallTool
 module.exports.PgDumpTool = nativeBinding.PgDumpTool
 module.exports.PgIsReadyTool = nativeBinding.PgIsReadyTool
