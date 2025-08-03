ba8447a529c921301f2535397b039d1a34b94722
Kingsword <kingsword09@gmail.com>
2025-08-03 08:21:19 +0800

    feat: add postgres tools pg_dumpall

---
+++ __test__/assets/dumpall.sql
@@ -0,0 +1,93 @@
+--
+-- PostgreSQL database cluster dump
+--
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
+--
+-- Roles
+--
+
+CREATE ROLE postgres;
+ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS PASSWORD 'SCRAM-SHA-256$4096:c+nh+vgvRhx1oS/SjTmNpA==$XsRk+Jnz06hFlzxppsb22zEeCyNwm4JQi5Y6T47AYpA=:I+KyyfyqpxyNHyFumk/i1cxiZ8FLB8BIVftXcIx5x2c=';
+
+--
+-- User Configurations
+--
+
+
+
+
+
+
+
+
+--
+-- Databases
+--
+
+--
+-- Database "template1" dump
+--
+
+\connect template1
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 17.5
+-- Dumped by pg_dump version 17.5
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- Database "postgres" dump
+--
+
+\connect postgres
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 17.5
+-- Dumped by pg_dump version 17.5
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- PostgreSQL database cluster dump complete
+--
+
--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -2,12 +2,14 @@
 
 pub mod common;
 pub mod pg_dump;
+pub mod pg_dumpall;
 pub mod pg_isready;
 pub mod pg_restore;
 pub mod psql;
 
 pub use self::common::*;
 pub use self::pg_dump::*;
+pub use self::pg_dumpall::*;
 pub use self::pg_isready::*;
 pub use self::pg_restore::*;
 pub use self::psql::*;
